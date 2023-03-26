3e956a0266a8d62392ff4d895cba489e80e526a5
carlaweglin <weglin.carla@gmail.com>
2023-03-26 19:39:45 -0300

    maquetado header basico

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Container, Heading } from '@chakra-ui/react'
+
 function App() {
   
 
   return (
     <div className="App">
-      
-      
+      <Container bg='lightBlue'  maxW='100%' height="100vh" p="50px" >
+      <Heading  fontSize='50px' >TODO LIST</Heading>
+      </Container>
     </div>
   )
 }
