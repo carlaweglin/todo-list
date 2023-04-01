6aa3f686468950b2c6d0f79bbfad0063a7210b7d
carlaweglin <weglin.carla@gmail.com>
2023-04-01 17:33:37 -0300

    se corrige estilado y se agrega iconos en botones

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <Container bg='lightBlue'  maxW='100%' height="100vh" pt="100px" >
+      <Container bg='lightBlue'  maxW='100%' minHeight="100vh" pt="100px" py='50px' >
       <header>
       <Heading as="h1" fontSize='50px' >TODO LIST</Heading>
       </header>
--- src/components/AgregarTarea.jsx
+++ src/components/AgregarTarea.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react';
-import { Text, Input, Box, Select, Flex, Spacer, Button } from "@chakra-ui/react";
-import { ListaTareas } from './ListaTareas';
+import { useState } from "react";
+import {
+  Text,
+  Input,
+  Box,
+  Select,
+  Flex,
+  Spacer,
+  Button,
+} from "@chakra-ui/react";
+import { ListaTareas } from "./ListaTareas";
 
 export function AgregarTarea() {
+  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState("");
 
-    const [tareas, setTareas] = useState([]);
-    const [tarea, setTarea] = useState('');
+  function handleChange(e) {
+    setTarea(e.target.value);
+  }
 
-    
-    function handleChange(e) {
-         setTarea(e.target.value)
-      }
-
-      function agregarTarea() {
-        let aux = tareas;
-        let tareaIngresada = {
-            descripcion : tarea,
-            id : self.crypto.randomUUID(),
-            
-        }
-        aux.push(tareaIngresada)
-        setTareas(aux)
-        setTarea('')
-     }
-
+  function agregarTarea() {
+    let aux = tareas;
+    let tareaIngresada = {
+      descripcion: tarea,
+      id: self.crypto.randomUUID(),
+    };
+    aux.push(tareaIngresada);
+    setTareas(aux);
+    setTarea("");
+  }
 
   return (
     <>
       <Flex m="100px 100px 50px 100px">
-        <Box w="45%">
+        <Box w="45%" boxShadow="-5px 1px 37px -3px rgba(0,0,0,0.52)">
+          <Text as="h2" fontSize="xl">
+            Tarea
+          </Text>
           <Input
             p="20px"
             bg="white"
@@ -39,7 +46,10 @@ export function AgregarTarea() {
           />
         </Box>
         <Spacer />
-        <Box w="45%">
+        <Box w="45%" boxShadow="-5px 1px 37px -3px rgba(0,0,0,0.52)">
+          <Text as="h2" fontSize="xl">
+            Filtros
+          </Text>
           <Select bg="white" placeholder="Seleccionar">
             <option value="todas">Todas</option>
             <option value="completas">Completas</option>
@@ -57,7 +67,7 @@ export function AgregarTarea() {
       >
         Agregar
       </Button>
-    <ListaTareas tareas={tareas}/>
+      <ListaTareas tareas={tareas} />
     </>
   );
 }
